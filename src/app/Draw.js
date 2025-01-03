ccd502ce69e8f468d6c189bf774533c59420e3e5
Olivia Yu <olivyu@gmail.com>
2025-01-02 17:06:34 -0800

    Add drawing app

---
+++ src/app/Draw.js
@@ -0,0 +1,96 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import Menu from "./components/Menu";
+import Image from "next/image";
+import "./draw.css";
+
+function Draw() {
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lineWidth, setLineWidth] = useState(5);
+  const [lineColor, setLineColor] = useState("black");
+  const [lineOpacity, setLineOpacity] = useState(0.1);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.globalAlpha = lineOpacity;
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = lineWidth;
+    ctxRef.current = ctx;
+  }, [lineColor, lineOpacity, lineWidth]);
+
+  const startDrawing = (e) => {
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+
+    ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    ctxRef.current.stroke();
+
+    setIsDrawing(true);
+  };
+
+  const endDrawing = () => {
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = (e) => {
+    if (isDrawing) {
+      ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+
+      ctxRef.current.stroke();
+    }
+  };
+
+  const clearDrawing = () => {
+    ctxRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+  };
+
+  return (
+    <div className="doodle-app flex flex-col justify-start items-center bg-gray-100">
+      <div className="flex">
+        <Image
+          className="dark:invert"
+          src="/brush.svg"
+          alt="Brush"
+          width={48}
+          height={48}
+        />
+        <h1 className="ml-2 font-sans font-medium text-5xl text-black py-4">
+          Doodle
+        </h1>
+      </div>
+
+      <div className="draw-area bg-white border-gray-200 border-2 relative">
+        <Menu
+          setLineColor={setLineColor}
+          setLineWidth={setLineWidth}
+          setLineOpacity={setLineOpacity}
+          clearDrawing={clearDrawing}
+        />
+
+        <canvas
+          onMouseDown={startDrawing}
+          onMouseUp={endDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          width={`1280px`}
+          height={`720px`}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Draw;
---
+++ src/app/components/Menu.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "../draw.css";
+
+const Menu = ({ clearDrawing, setLineColor, setLineWidth, setLineOpacity }) => {
+  return (
+    <div className="menu flex border-4 justify-evenly rounded my-4 items-center mx-auto text-zinc-900 font-light">
+      <div className="flex">
+        <label>Brush Color</label>
+        <input
+          className="ml-2"
+          type="color"
+          onChange={(e) => {
+            setLineColor(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="flex">
+        <label>Brush Width</label>
+        <input
+          className="ml-2"
+          type="range"
+          min="3"
+          max="20"
+          onChange={(e) => {
+            setLineWidth(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="flex">
+        <label>Brush Opacity</label>
+        <input
+          className="ml-2"
+          type="range"
+          min="1"
+          max="100"
+          onChange={(e) => {
+            setLineOpacity(e.target.value / 100);
+          }}
+        />
+      </div>
+
+      <div>
+        <button
+          aria-label="Clear Drawing"
+          className="py-1 px-3"
+          onClick={clearDrawing}
+        >
+          Clear
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
